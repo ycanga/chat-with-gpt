4af263221ed2facd65d46441f3a2cdaac1921340
Ycanga <canayunus@gmail.com>
2024-06-20 14:25:50 +0300

    Start Bash File added.

--- start.sh
+++ start.sh
@@ -1,33 +1,20 @@
-# #!/bin/bash
-
-# # Kullanıcıdan API anahtarını alma
-# read -p $'\e[33mEnter your OpenAI API key: \e[0m' api_key
-
-# # config.js dosyasının yolunu belirtin
-# config_file="src/config.js"
-
-# # Eski API anahtarını yeni anahtarla değiştirme
-# sed -i.bak "s|let ApiKey = .*|let ApiKey = \"$api_key\";|" $config_file
-
-# echo $'\e[32mAPI key has been updated in\e[0m' $config_file
-
 #!/bin/bash
 
-# Kullanıcıdan API anahtarını alma
+# Getting the API key from the user
 read -p $'\e[33mEnter your OpenAI API key: \e[0m' api_key
 
-# config.js dosyasının yolunu belirtin
+# Specify the path to the config.js file
 config_file="src/config.js"
 
-# Eski API anahtarını yeni anahtarla değiştirme
+# Replacing the old API key with the new key
 sed -i.bak "s|let ApiKey = .*|let ApiKey = \"$api_key\";|" $config_file
 
 echo $'\e[32mAPI key has been updated in\e[0m' $config_file
 
-# npm install komutunu çalıştırma
+# Run the npm install command
 echo $'\e[34mRunning npm install...\e[0m'
 npm install
 
-# npm start komutunu çalıştırma
+# Run the npm start command
 echo $'\e[34mRunning npm start...\e[0m'
 npm start
